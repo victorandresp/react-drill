2e045c04d36fa6f2948fe6a585d03947a9338597
victorandresp <ponceottamendi27@gmail.com>
2025-02-19 23:11:21 -0400

    feat: styles card :lipstick:

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,17 +1,18 @@
-import "./index.css"
+import "./App.css"
+
 export const App = () => {
     const title = "Boton";
     return (
-        <article>
-            <header>
-                <img src="https://unavatar.io/github/mdo" alt="avatar   " />
-                <div>
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img className="tw-followCard-avatar" src="https://unavatar.io/github/mdo" alt="avatar" />
+                <div className="tw-followCard-info">
                     <strong>Victor Andres Ponce</strong>
-                    <span>@midudev</span>
+                    <span className="tw-followCard-infoUserName">@victorandresp</span>
                 </div>
             </header>
             <aside>
-                <button>
+                <button className="tw-followCard-button">
                     Seguir
                 </button>
             </aside>
