5f235ece751734a48e0970b53249b4d5fce71472
victorandresp <ponceottamendi27@gmail.com>
2025-02-20 23:12:23 -0400

    feat: verified badge and element html passed by prop :sparkles:

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,14 +1,27 @@
 import "./App.css"
-import { Fragment } from "react";
 import { TwitterFollowCard }  from "./components/TwitterFollowCard.jsx";
 
 export const App = () => {
     const format = (userName) => `@${userName}`
+    const verifiedButtonElement = <img src="verified.svg" width={20} /> // elements can be passed by props to childs components
     return (
-        <Fragment>
-            <TwitterFollowCard formatUserName={format} fullName="Victor Andres Ponce" userName="victorandresp" />
-            <TwitterFollowCard formatUserName={format} fullName="Miguel Angel Duran" userName="midudev" />
-            <TwitterFollowCard formatUserName={format} fullName="Kai Cenat" userName="kaicenat" />
-        </Fragment>
+        <>
+            <TwitterFollowCard 
+                verifiedBadge={verifiedButtonElement}
+                formatUserName={format} 
+                fullName="Victor Andres Ponce" 
+                userName="victorandresp" 
+            />
+            <TwitterFollowCard
+                formatUserName={format}
+                fullName="Miguel Angel Duran"
+                userName="midudev"
+            />
+            <TwitterFollowCard
+                formatUserName={format}
+                fullName="Kai Cenat"
+                userName="kaicenat"
+            />
+        </>
     )
 }
--- projects/00-hola-mundo/src/components/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/components/TwitterFollowCard.jsx
@@ -1,10 +1,10 @@
-export const TwitterFollowCard = ({formatUserName, fullName, userName, isFollowing = false}) => {
+export const TwitterFollowCard = ({verifiedBadge, formatUserName, fullName, userName, isFollowing = false}) => {
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
                 <img className="tw-followCard-avatar" src={`https://unavatar.io/twitter/${userName}`} alt="avatar" />
                 <div className="tw-followCard-info">
-                    <strong>{ fullName }</strong>
+                    <strong>{ fullName }<span className="tw-followCard-badge">{verifiedBadge}</span></strong>
                     <span className="tw-followCard-infoUserName">{ formatUserName(userName) }</span>
                 </div>
             </header>
