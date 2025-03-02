1711b73c0bf26b578d3ff76b05772f21ea515993
victorandresp <ponceottamendi27@gmail.com>
2025-03-01 22:01:30 -0400

    feat: pass children component :sparkles:

--- projects/00-hola-mundo/src/components/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/components/TwitterFollowCard.jsx
@@ -1,10 +1,11 @@
-export const TwitterFollowCard = ({verifiedBadge, formatUserName, fullName, userName, isFollowing = false}) => {
+
+export const TwitterFollowCard = ({children, verifiedBadge, formatUserName, fullName, userName, isFollowing = false}) => {
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
                 <img className="tw-followCard-avatar" src={`https://unavatar.io/twitter/${userName}`} alt="avatar" />
                 <div className="tw-followCard-info">
-                    <strong>{ fullName }<span className="tw-followCard-badge">{verifiedBadge}</span></strong>
+                    <strong>{ children }<span className="tw-followCard-badge">{verifiedBadge}</span></strong>
                     <span className="tw-followCard-infoUserName">{ formatUserName(userName) }</span>
                 </div>
             </header>
