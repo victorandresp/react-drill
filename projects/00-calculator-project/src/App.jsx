eb741675d9533d9c061e398abb6232726fbbd7e3
victorandresp <ponceottamendi27@gmail.com>
2025-03-21 23:20:31 -0400

    chore: added comments for timer tracker and personal record :memo:

--- projects/00-calculator-project/src/App.jsx
+++ projects/00-calculator-project/src/App.jsx
@@ -4,9 +4,12 @@ import Operation from './components/Operation'
 import Result from './components/Result'
 import './App.css'
 
+// Personal record: 54:01.82
+// Get better: installation time: 9 min
+
 function App() {
   const [ numberList, setNumberList ] = useState([])
-  
+
   const numberPressed = (number) => {
     setNumberList([...numberList, number])
   }
