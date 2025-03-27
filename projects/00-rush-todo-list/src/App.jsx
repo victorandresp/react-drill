f1cde92ed154d54702239ec48639d33d239098b2
victorandresp <ponceottamendi27@gmail.com>
2025-03-26 23:34:19 -0400

    chore: theory rush :memo:

--- projects/00-rush-todo-list/src/App.jsx
+++ projects/00-rush-todo-list/src/App.jsx
@@ -4,6 +4,11 @@ import TodoList from "./components/TodoList"
 import './App.css'
 
 // best time: 01:16:53
+
+// Theory snippet: 'Batching'
+// React se encarga de ejecutar en lote (batch) el cambio de un estado para actualizar el DOM una sola vez
+// Por lo que si se llama un setState() varias veces en una funcion, solo se va a actualizar una sola vez porque 
+// se hace por lotes la actualizacion de ese dicho estado. 
 function App() {
   const [items, setItems] = useState([])
   const onTextSaved = (textSaved) =>{
