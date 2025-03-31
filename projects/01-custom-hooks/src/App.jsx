0db581cdb28a18db9de7bbd21bf1b0aa07102cf5
victorandresp <ponceottamendi27@gmail.com>
2025-03-30 23:59:11 -0400

    feat: called hook in view :sparkles:

--- projects/01-custom-hooks/src/App.jsx
+++ projects/01-custom-hooks/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useFetch } from '../hooks'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const url = "https://meowfacts.herokuapp.com/"
+  const { data, error, loading } = useFetch(url)
+
+  const getFunction = () =>{
+    if(loading){
+      return (<h2>Cargando...</h2>)
+    }
+    if(error){
+      return (<h2>UPS! existe un error: {error.message}</h2>)
+    }
+
+    return (<h1>{JSON.stringify(data)}</h1>)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {getFunction()}
     </>
   )
 }
